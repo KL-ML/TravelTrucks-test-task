25e8da0fba59ac88101624a533ac22423c8cfcba
KL-ML <knataly0513@gmail.com>
2025-06-04 16:10:47 +0300

    restor MyTextInput component

---
+++ src/components/MyTextInput/MyTextInput.jsx
@@ -0,0 +1,27 @@
+import { useField } from 'formik';
+import css from './MyTextInput.module.css';
+import clsx from 'clsx';
+
+
+export default function MyTextInput({
+  // eslint-disable-next-line no-unused-vars
+  tag: Tag = 'input',
+  variant,
+  label,
+  ...props
+}) {
+  const [field, meta] = useField(props);
+  return (
+    <div className={css.inputWrap}>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <Tag
+        className={clsx(css.textInput, css[variant])}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+}
