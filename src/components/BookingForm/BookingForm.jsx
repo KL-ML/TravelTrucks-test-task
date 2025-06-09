bf6ca08a6e36df603823f63a521c29e91771a0e6
KL-ML <knataly0513@gmail.com>
2025-06-09 14:43:09 +0300

    add datepicker custom styling

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,11 +1,79 @@
-import { Form, Formik } from 'formik';
+import { Form, Formik, useField } from 'formik';
+import * as Yup from 'yup';
+import DatePicker, { CalendarContainer } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import css from './BookingForm.module.css';
 import Heading from '../Heading/Heading';
 import MyTextInput from '../MyTextInput/MyTextInput';
 import Description from '../Description/Description';
 import Button from '../Button/Button';
+import { forwardRef } from 'react';
+
+//  контейнер
+
+// инпут
+const ExampleCustomInput = forwardRef(({ value, onClick, className }, ref) => (
+  <button className={css.dateTextInput} onClick={onClick} ref={ref}>
+    {value}
+  </button>
+));
+
+// day
+
+// головний компонент
+const MyDatePicker = ({ name = '' }) => {
+  const [field, meta, helpers] = useField(name);
+
+  const { value } = meta;
+  const { setValue } = helpers;
+
+  function handleDayClassName(date) {
+    if (date.getDay() === 0) {
+      return 'sunday'; // Add a class for Sundays
+    } else if (date.getDay() === 6) {
+      return 'saturday'; // Add a class for Saturdays
+    }
+    return '';
+  }
+
+  return (
+    <div className={css.inputWrap}>
+      <DatePicker
+        placeholderText="Booking date *"
+        dateFormat="dd/MM/yyyy"
+        {...field}
+        selected={value}
+        onChange={date => setValue(date)}
+        calendarClassName={css.calendar}
+        className={css.dateTextInput}
+        dayClassName={handleDayClassName}
+      />
+    </div>
+  );
+};
 
 export default function BookingForm() {
+  function handleSubmit(values, actions) {
+    console.log(values);
+    alert(JSON.stringify(values.bookingDate));
+    actions.resetForm();
+  }
+  const BookingSchema = Yup.object().shape({
+    name: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required(),
+    email: Yup.string().email('Invalid email address').required(),
+    bookingDate: Yup.string(),
+    comment: Yup.string(),
+  });
+
+  const defaultFormValues = {
+    name: '',
+    email: '',
+    bookingDate: new Date(),
+    comment: '',
+  };
+
   return (
     <div className={css.formWrap}>
       <div>
@@ -17,34 +85,37 @@ export default function BookingForm() {
         </Description>
       </div>
       <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          bookingDate: '',
-          comment: '',
-        }}
+        initialValues={defaultFormValues}
+        onSubmit={handleSubmit}
+        validationSchema={BookingSchema}
       >
-        <Form className={css.bookingForm} id="bookingForm">
-          <MyTextInput name="name" type="text" placeholder="Name *" />
-          <MyTextInput name="email" type="email" placeholder="Email *" />
-          <MyTextInput
-            name="date"
-            type="text"
-            placeholder="Booking date *"
-            variant="date"
-          />
-          <MyTextInput
-            tag="textarea"
-            name="comment"
-            placeholder="Comment "
-            variant="textarea"
-          />
-        </Form>
+        {props => (
+          <Form className={css.bookingForm} id="bookingForm">
+            <MyTextInput name="name" type="text" placeholder="Name *" />
+            <MyTextInput name="email" type="email" placeholder="Email *" />
+            <MyDatePicker
+              name="bookingDate"
+              type="date"
+              variant="date"
+            />
+            <MyTextInput
+              tag="textarea"
+              name="comment"
+              placeholder="Comment "
+              variant="textarea"
+            />
+            <div className={css.btnWrap}>
+              <Button
+                handleSubmit={handleSubmit}
+                form="bookingForm"
+                btnType="submit"
+                variant="filled"
+                text="Send"
+              />
+            </div>
+          </Form>
+        )}
       </Formik>
-      <div className={css.btnWrap}>
-        <Button form="bookingForm" type="submit" variant="filled" text="Send" />
-      </div>
-      {/* <Button form="bookingForm" type="submit" variant="filled" text="Send" /> */}
     </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,17 +3,36 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import css from './Button.module.css';
 
-export default function Button({ text, variant, btnType, disabled, handleClick }) {
+export default function Button({
+  text,
+  variant,
+  btnType,
+  disabled,
+  handleClick,
+  handleSubmit,
+}) {
   return (
     <>
-      <button
-        disabled={disabled}
-        type={btnType}
-        className={clsx(css.button, css[variant])}
-        onClick={handleClick}
-      >
-        {text}
-      </button>
+      {btnType === 'button' && (
+        <button
+          disabled={disabled}
+          type={btnType}
+          className={clsx(css.button, css[variant])}
+          onClick={handleClick}
+        >
+          {text}
+        </button>
+      )}
+      {btnType === 'submit' && (
+        <button
+          disabled={disabled}
+          type={btnType}
+          className={clsx(css.button, css[variant])}
+          onSubmit={handleSubmit}
+        >
+          {text}
+        </button>
+      )}
     </>
   );
 }
