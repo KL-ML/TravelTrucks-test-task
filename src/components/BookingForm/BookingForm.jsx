47398e6f360ae91b078b07c1ddfb567298470f1c
KL-ML <knataly0513@gmail.com>
2025-06-09 17:31:34 +0300

    create MyDatePicker component

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,69 +1,27 @@
-import { Form, Formik, useField } from 'formik';
+import { Form, Formik} from 'formik';
 import * as Yup from 'yup';
-import DatePicker, { CalendarContainer } from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import css from './BookingForm.module.css';
 import Heading from '../Heading/Heading';
 import MyTextInput from '../MyTextInput/MyTextInput';
 import Description from '../Description/Description';
 import Button from '../Button/Button';
-import { forwardRef } from 'react';
+import MyDatePicker from '../MyDatePicker/MyDatePicker';
+import toast, { Toaster } from 'react-hot-toast';
 
-//  контейнер
 
-// инпут
-const ExampleCustomInput = forwardRef(({ value, onClick, className }, ref) => (
-  <button className={css.dateTextInput} onClick={onClick} ref={ref}>
-    {value}
-  </button>
-));
-
-// day
-
-// головний компонент
-const MyDatePicker = ({ name = '' }) => {
-  const [field, meta, helpers] = useField(name);
-
-  const { value } = meta;
-  const { setValue } = helpers;
-
-  function handleDayClassName(date) {
-    if (date.getDay() === 0) {
-      return 'sunday'; // Add a class for Sundays
-    } else if (date.getDay() === 6) {
-      return 'saturday'; // Add a class for Saturdays
-    }
-    return '';
-  }
-
-  return (
-    <div className={css.inputWrap}>
-      <DatePicker
-        placeholderText="Booking date *"
-        dateFormat="dd/MM/yyyy"
-        {...field}
-        selected={value}
-        onChange={date => setValue(date)}
-        calendarClassName={css.calendar}
-        className={css.dateTextInput}
-        dayClassName={handleDayClassName}
-      />
-    </div>
-  );
-};
 
 export default function BookingForm() {
   function handleSubmit(values, actions) {
     console.log(values);
-    alert(JSON.stringify(values.bookingDate));
+    toast(
+      `Dear ${values.name}! Your order was successful!. You will receive a booking confirmation via email ${values.email} shortly.`
+    );
     actions.resetForm();
   }
   const BookingSchema = Yup.object().shape({
     name: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required(),
     email: Yup.string().email('Invalid email address').required(),
-    bookingDate: Yup.string(),
+    bookingDate: Yup.date('Invalid date format').required(),
     comment: Yup.string(),
   });
 
@@ -89,33 +47,34 @@ export default function BookingForm() {
         onSubmit={handleSubmit}
         validationSchema={BookingSchema}
       >
-        {props => (
-          <Form className={css.bookingForm} id="bookingForm">
-            <MyTextInput name="name" type="text" placeholder="Name *" />
-            <MyTextInput name="email" type="email" placeholder="Email *" />
-            <MyDatePicker
-              name="bookingDate"
-              type="date"
-              variant="date"
+        <Form className={css.bookingForm} id="bookingForm">
+          <MyTextInput name="name" type="text" placeholder="Name *" />
+          <MyTextInput name="email" type="email" placeholder="Email *" />
+          <MyDatePicker name="bookingDate" type="date" variant="date" />
+          <MyTextInput
+            tag="textarea"
+            name="comment"
+            placeholder="Comment "
+            variant="textarea"
+          />
+          <div className={css.btnWrap}>
+            <Button
+              handleSubmit={handleSubmit}
+              form="bookingForm"
+              btnType="submit"
+              variant="filled"
+              text="Send"
             />
-            <MyTextInput
-              tag="textarea"
-              name="comment"
-              placeholder="Comment "
-              variant="textarea"
-            />
-            <div className={css.btnWrap}>
-              <Button
-                handleSubmit={handleSubmit}
-                form="bookingForm"
-                btnType="submit"
-                variant="filled"
-                text="Send"
-              />
-            </div>
-          </Form>
-        )}
+          </div>
+        </Form>
       </Formik>
+      <Toaster
+        containerClassName={css.toaster}
+        toastOptions={{
+          className: css.bookingToast,
+          removeDelay: 2000,
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/MyDatePicker/MyDatePicker.jsx
@@ -0,0 +1,26 @@
+import css from './MyDatePicker.module.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import { useField } from "formik";
+
+export default function MyDatePicker({ name = '' }) {
+  const [field, meta, helpers] = useField(name);
+
+  const { value } = meta;
+  const { setValue } = helpers;
+
+  return (
+    <div className={css.inputWrap}>
+      <DatePicker
+        placeholderText="Booking date *"
+        dateFormat="dd/MM/yyyy"
+        {...field}
+        selected={value}
+        onChange={date => setValue(date)}
+        calendarClassName={css.calendar}
+        className={css.dateTextInput}
+      />
+    </div>
+  );
+};
