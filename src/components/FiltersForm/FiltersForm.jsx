c53a0ece9a150afe84b2b411e98cf4aa813ce56c
KL-ML <knataly0513@gmail.com>
2025-06-09 23:50:34 +0300

    add adaptive styles

--- src/components/FiltersForm/FiltersForm.jsx
+++ src/components/FiltersForm/FiltersForm.jsx
@@ -66,7 +66,7 @@ export default function FiltersForm() {
         validationSchema={filtersSchema}
       >
         {({ isSubmitting }) => (
-          <Form>
+          <Form className={css.formWrap}>
             {/* Location select */}
             <div className={css.selectWrap}>
               <label className={css.selectLable} htmlFor="city">
@@ -109,222 +109,276 @@ export default function FiltersForm() {
                 </Field>
               </div>
             </div>
-
-            <Heading tag="h2" variant="filtersB2" textColor>
-              Filters
-            </Heading>
-
-            {/* Vehicle equipment checkbox */}
-            <div className={css.checkboxGroup}>
-              <Heading
-                tag="h3"
-                variant="headerH3"
-                underlined
-                id="checkbox-group"
-              >
-                Vehicle equipment
+            <div>
+              <Heading tag="h2" variant="filtersB2" textColor>
+                Filters
               </Heading>
-              <div
-                className={css.checkboxWrap}
-                role="group"
-                aria-labelledby="checkbox-group"
-              >
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="AC"
-                    id={ACFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#ac`}></use>
-                    </svg>
-                    AC
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="bathroom"
-                    id={bathroomFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#bathroom`}></use>
-                    </svg>
-                    Bathroom
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="kitchen"
-                    id={kitchenFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#kitchen`}></use>
-                    </svg>
-                    Kitchen
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="TV"
-                    id={TVFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#tv`}></use>
-                    </svg>
-                    TV
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="radio"
-                    id={radioFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#radio`}></use>
-                    </svg>
-                    Radio
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="refrigerator"
-                    id={refrigeratorFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#refrigerator`}></use>
-                    </svg>
-                    Refrigerator
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="microwave"
-                    id={microwaveFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#microwave`}></use>
-                    </svg>
-                    Microwave
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="gas"
-                    id={gasFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#gas`}></use>
-                    </svg>
-                    Gas
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="checkbox"
-                    name="equipment"
-                    value="water"
-                    id={waterFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#water`}></use>
-                    </svg>
-                    Water
-                  </span>
-                </label>
+              <div className={css.filtersWrap}>
+                {/* Vehicle equipment checkbox */}
+                <div className={css.checkboxGroup}>
+                  <Heading
+                    tag="h3"
+                    variant="headerH3"
+                    underlined
+                    id="checkbox-group"
+                  >
+                    Vehicle equipment
+                  </Heading>
+                  <div
+                    className={css.checkboxWrap}
+                    role="group"
+                    aria-labelledby="checkbox-group"
+                  >
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="AC"
+                        id={ACFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#ac`}></use>
+                        </svg>
+                        AC
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="bathroom"
+                        id={bathroomFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#bathroom`}></use>
+                        </svg>
+                        Bathroom
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="kitchen"
+                        id={kitchenFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#kitchen`}></use>
+                        </svg>
+                        Kitchen
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="TV"
+                        id={TVFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#tv`}></use>
+                        </svg>
+                        TV
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="radio"
+                        id={radioFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#radio`}></use>
+                        </svg>
+                        Radio
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="refrigerator"
+                        id={refrigeratorFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#refrigerator`}></use>
+                        </svg>
+                        Refrigerator
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="microwave"
+                        id={microwaveFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#microwave`}></use>
+                        </svg>
+                        Microwave
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="gas"
+                        id={gasFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#gas`}></use>
+                        </svg>
+                        Gas
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="checkbox"
+                        name="equipment"
+                        value="water"
+                        id={waterFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#water`}></use>
+                        </svg>
+                        Water
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+                {/* Vehicle type radio */}
+                <div className={css.radioGroup}>
+                  <Heading
+                    tag="h3"
+                    variant="headerH3"
+                    underlined
+                    id="radio-group"
+                  >
+                    Vehicle type
+                  </Heading>
+                  <div
+                    className={css.checkboxWrap}
+                    role="group"
+                    aria-labelledby="radio-group"
+                  >
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="radio"
+                        name="form"
+                        value="panelTruck"
+                        id={panelTruckFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#van`}></use>
+                        </svg>
+                        Van
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="radio"
+                        name="form"
+                        value="fullyIntegrated"
+                        id={fullyIntegratedFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#fullyintegrated`}></use>
+                        </svg>
+                        Fully Integrated
+                      </span>
+                    </label>
+                    <label className={css.checkboxLable}>
+                      <Field
+                        className={css.checkfield}
+                        type="radio"
+                        name="form"
+                        value="alcove"
+                        id={alcoveFieldId}
+                      />
+                      <span className={css.checkmark}>
+                        <svg
+                          className={css.categoryIcon}
+                          width="32"
+                          height="32"
+                        >
+                          <use href={`${iconsPath}#alcove`}></use>
+                        </svg>
+                        Alcove
+                      </span>
+                    </label>
+                  </div>
+                </div>
               </div>
             </div>
-
-            {/* Vehicle type radio */}
-            <div className={css.radioGroup}>
-              <Heading tag="h3" variant="headerH3" underlined id="radio-group">
-                Vehicle type
-              </Heading>
-              <div
-                className={css.checkboxWrap}
-                role="group"
-                aria-labelledby="radio-group"
-              >
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="radio"
-                    name="form"
-                    value="panelTruck"
-                    id={panelTruckFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#van`}></use>
-                    </svg>
-                    Van
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="radio"
-                    name="form"
-                    value="fullyIntegrated"
-                    id={fullyIntegratedFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#fullyintegrated`}></use>
-                    </svg>
-                    Fully Integrated
-                  </span>
-                </label>
-                <label className={css.checkboxLable}>
-                  <Field
-                    className={css.checkfield}
-                    type="radio"
-                    name="form"
-                    value="alcove"
-                    id={alcoveFieldId}
-                  />
-                  <span className={css.checkmark}>
-                    <svg className={css.categoryIcon} width="32" height="32">
-                      <use href={`${iconsPath}#alcove`}></use>
-                    </svg>
-                    Alcove
-                  </span>
-                </label>
-              </div>
-            </div>
-
             <Button
               variant="filled"
               text="Submit"
